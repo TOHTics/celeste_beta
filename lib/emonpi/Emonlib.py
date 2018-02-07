eb2563a2beb623931586e811bb686581381e415b
Rajiv91 <rajivgg91@gmail.com>
2018-02-07 09:07:05 -0600

    Versión estable, la parte del sensor de voltaje, hay un error de +-3v esperemos se corrija con el pcb

--- lib/emonpi/Emonlib.py
+++ lib/emonpi/Emonlib.py
@@ -50,40 +50,57 @@ class EnergyMonitor:
         self.startV=0
 
     #el numero de muestras esta definido por el numero de cruces (numero de medias ondas)
-    def calcVI(self, crossings, timeout):
+    def calcVI(self, crossings, timeout, voltSensor):
         SupplyVoltage=3300#puede leer del adc
         crossCount=0
         numberOfSamples=0
+        #print "voltSensor = ", voltSensor
 
   #//-------------------------------------------------------------------------------------------------------------------------
   #// 1) Waits for the waveform to be close to 'zero' (mid-scale adc) part in sin curve.
   #//-------------------------------------------------------------------------------------------------------------------------
         st=False; #an indicator to exit the while loop
-        start=time.clock()#Para que no se quede atorado en el loop
-        print "Detectando que la signal este cercana a cero"
-        while st==False:
+        start=time.time()#Para que no se quede atorado en el loop
+        #print "Detectando que la signal este cercana a cero"
+        countZero=0 #Contador de veces que esta dentro del rango cero
+        while st==False and voltSensor==True:
             self.startV=self.mcp.read_adc(self.channelV)
+            #print "self.startV = ", self.startV
+            #print "startV = ", self.startV
             if self.startV<(self.ADC_COUNTS*0.55) and self.startV>(self.ADC_COUNTS*0.45):#Checa si esta en el rango (cercano al cero)
+                countZero+=1
+                #print "Detectado"
+            if countZero==1:
+             #   print "detectada"
                 st=True
-            elapsedTime=time.clock()-start
-            #print elapsedTime
+            elapsedTime=time.time()-start
             if elapsedTime>timeout:
+                #print "elapsedTime timeout"
+                #self.startV=0
                 st=True
-        print "Detectado"
+            #time.sleep(.001)
 
-        start=time.clock()
+        start=time.time()
         crossCount=0
         nSamples=0
-        while crossCount<crossings and (time.clock()-start)<timeout:
+        timeout2=timeout
+        print "primera lectura adc: ", self.startV
+        while crossCount<crossings and voltSensor==True: #Hasta ahora los calculos son erroneos puede ser por la frecuencia de muestreo
+            if(time.time()-start)>=timeout2 and voltSensor==True:
+                #self.sumV=0
+                print "timeout!"
+                break
             nSamples+=1
             self.lastFilteredV=self.filteredV
             self.sampleV=self.mcp.read_adc(self.channelV)
             self.sampleI=self.mcp.read_adc(self.channelI)
         
             #Se aplica un flitro digital pasabajas para extraer, y se quita el offset de 1.65
-            self.offsetV=self.offsetV+((self.sampleV-self.offsetV)/1024)
+            self.offsetV=self.offsetV+((self.sampleV-self.offsetV)/1024.0)
+
+            #print "offsetV dentro del while", self.offsetV
             self.filteredV=self.sampleV-self.offsetV
-            self.offsetI=self.offsetI+((self.sampleI-self.offsetI)/1024)
+            self.offsetI=self.offsetI+((self.sampleI-self.offsetI)/1024.0)
             self.filteredI=self.sampleI-self.offsetI
 
             #Voltaje root-mean-square
@@ -112,23 +129,43 @@ class EnergyMonitor:
                 self.lastVCross=self.checkVCross
             if self.lastVCross!=self.checkVCross:
                 crossCount+=1
+            if crossCount==crossings:
+                print "maximo de cruces!"
+                #self.sumV=0
+            #time.sleep(.001)
+        print "paso por el voltaje inicial ", crossCount
+        print "muestras", nSamples
+        #print "offsetV", self.offsetV
+        #print "lastSample ", self.sampleV
+        #print "filteredV ", self.filteredV
                  
+        #print "sumV = ", (self.sumV/nSamples)
         #post loop calculations
-        V_RATIO=self.calibrationV*((SupplyVoltage/1000.0)/self.ADC_COUNTS)
-        self.Vrms=V_RATIO*math.sqrt(self.sumV/nSamples)
-        print "Vrms", self.Vrms
+        if(voltSensor==False):
+            self.realPower=0
+            self.apparentPower=0
+            self.Vrms=0
+        else: 
+            V_RATIO=self.calibrationV*((SupplyVoltage/1000.0)/self.ADC_COUNTS)
+            self.Vrms=V_RATIO*math.sqrt(self.sumV/nSamples)
+            #print "Vrms", self.Vrms
 
-        I_RATIO=self.calibrationI*((SupplyVoltage/1000.0)/self.ADC_COUNTS)
-        self.Irms=I_RATIO*math.sqrt(self.sumI/nSamples)
+            I_RATIO=self.calibrationI*((SupplyVoltage/1000.0)/self.ADC_COUNTS)
+            self.Irms=I_RATIO*math.sqrt(self.sumI/nSamples)
 
-        #Calculation power values
-        self.realPower=V_RATIO*I_RATIO*self.sumP/nSamples
-        self.apparentPower=self.Vrms*self.Irms
-        self.powerFactor=self.realPower/self.apparentPower
+            #Calculation power values
+            self.realPower=V_RATIO*I_RATIO*self.sumP/nSamples
+            self.apparentPower=self.Vrms*self.Irms
+        #self.powerFactor=self.realPower/self.apparentPower
 
-        self.SumV=0
-        self.SumI=0
-        self.SumP=0
+        print "Vrms = ", self.Vrms
+
+
+        self.sumV=0
+        self.sumI=0
+        self.sumP=0
+        self.filteredV=0
+        #self.offsetV=0
 
 
 
--- test/adcTest.py
+++ test/adcTest.py
@@ -3,6 +3,24 @@
 import Adafruit_GPIO.SPI as SPI
 import Adafruit_MCP3008
 import imp
+import RPi.GPIO as GPIO  
+import time
+GPIO.setmode(GPIO.BCM)  
+
+voltSensor=True #Para indicar que esta conectado el sensor
+def my_callback(channel):  
+    print " edge detected on port : ", channel
+    print "- even though, in the main thread,"  
+    print "waiting..."
+    global voltSensor
+    voltSensor= GPIO.input(21)
+    if voltSensor==True:
+        print "Rising edge"
+    else:
+        print "Falling detection"
+
+    #time.sleep(10)
+    #print "we are still waiting for a falling edge - how cool?\n"  
 
 
 if __name__ == "__main__":
@@ -13,7 +31,22 @@ if __name__ == "__main__":
     myMcp=Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
     emon1=Emon.EnergyMonitor(myMcp)
 
-    emon1.setVoltage(0, 1500, 1.6)
+    GPIO.setup(21, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) #Se activa la pulldown interna    
+    voltSensor= GPIO.input(21)
+    print "voltSensor = ", voltSensor
+    GPIO.add_event_detect(21, GPIO.BOTH, callback=my_callback)#Se declara la interrupcion
+    print "sale"
+    #GPIO.add_event_detect(21, GPIO.RISING, callback=my_callback2)#Se declara la interrupcion
+
+    emon1.setVoltage(0, 250, 1.6)
     emon1.setCurrent(1, 90.9)
-    emon1.calcVI(80, 2)
+
+    while True:
+        #emon1.setVoltage(0, 250, 1.6)
+        #print "inside while = ", voltSensor
+        #emon1.calcVI(260, 5, voltSensor)
+        emon1.calcVI(500, 10, voltSensor)
+        time.sleep(.5)
+
+
 
